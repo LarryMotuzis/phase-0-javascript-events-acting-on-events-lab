bed883d9c725172b48e2caf89cead44f65edcda7
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-18 12:40:46 -0600

    commit tree 23067eb9a109f635d35988e2da1d8d61d5d7ca47

--- index.js
+++ index.js
@@ -7,21 +7,15 @@ document.addEventListener("keydown", function (e) {
 });
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-      
-    }
-  }
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
 
+  dodger.style.left = `${left - 1}px`
+}
 
-function moveDodgerRight()  {
-    const RightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(RightNumbers, 10);
+function moveDodgerRight () {
+  const rightNumbers = dodger.style.left.replace('px', '');
+  const right = parseInt(rightNumbers, 10)
 
-    if (right > 0) {
-        dodger.style.left = `${right + 1}px`;
-    }
+  dodger.style.left = `${right + 1}px`
 }
